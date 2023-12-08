58df08522684032778402ee01a6cae6ad3aeddc9
Akhilesh yadav <akhileshyadav310596@gmail.com>
2023-12-09 02:09:46 +0530

    signup page

--- my-app/src/app/(public)/signup/form.tsx
+++ my-app/src/app/(public)/signup/form.tsx
@@ -10,22 +10,29 @@ const Form = () => {
     const router=useRouter()
     const [username,setUsername] = useState<undefined | string>("")
     const [password,setPassword] = useState<undefined | string>("")
-    
+    const [confirmPassword,setConfirmPassword] = useState<undefined | string>("")
 
-    
+    const [error,setError]=useState<string[]>([])
+
     const handleSubmit=async(e:FormEvent)=>{
-
+        
         e.preventDefault()
-        const res= await fetch("/api/login",{
+
+        if( password !=confirmPassword ){
+
+            error.push("Password do not match!")
+        }
+
+        const res= await fetch("/api/signup",{
             method: "post",
            body: JSON.stringify({username,password})
         })
 
         // console.log(res)
         if(res.ok){
-            router.push("/feed")
+            router.push("/signin")
         }else{
-            alert("Log in failed")
+            alert("Sign up failed")
         }
     }
   return (
@@ -34,13 +41,13 @@ const Form = () => {
      className="flex flex-col gap-2 p-5 bg-slate-800 max-w-xs w-full rounded-lg">
 
 <div  className="text-center">
-    <h3 className="font-semibold text-white">Sign In</h3>
+    <h3 className="font-semibold text-white">Sign Up</h3>
 </div>
 <div className="my-3">
     <hr/>
 </div>
 <div className="flex flex-col gap-2 ">
-    <label htmlFor="">Username</label>
+    <label className="text-white">Username</label>
     <input  type="text" 
     placeholder="Username"
     id="username"
@@ -52,7 +59,7 @@ const Form = () => {
     />
     </div>
     <div className="flex flex-col gap-2 ">
-        <label htmlFor="">Password</label>
+        <label className="text-white">Password</label>
         <input type="text"
          placeholder="Password" 
          id="password"
@@ -62,9 +69,19 @@ const Form = () => {
          className="text-block p-2 border border-slate-400 rounded-lg"
          />
         </div>
-       
+        <div className="flex flex-col gap-2 ">
+        <label className="text-white"> Confirm Password</label>
+        <input type="text"
+         placeholder="Password" 
+         id="password"
+         value={confirmPassword}
+         onChange={(e)=>setConfirmPassword(e.target.value)}
+         required
+         className="text-block p-2 border border-slate-400 rounded-lg"
+         />
+        </div>
  <div className="flex flex-col gap-2 ">
-    <button type="submit" className="bg-slate-400 p-3 mt-4  rounded-lg ">Signin</button>
+    <button type="submit" className="bg-slate-400 p-3 mt-4  rounded-lg ">Signup</button>
  </div>
     </form>
     </>
