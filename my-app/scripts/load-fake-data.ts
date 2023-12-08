2abf0d3d316ba0aa5f805e2c18f028880cf08b12
Akhilesh yadav <akhileshyadav310596@gmail.com>
2023-12-08 14:13:47 +0530

    make connection

---
+++ my-app/scripts/load-fake-data.ts
@@ -0,0 +1,29 @@
+import { Client } from "pg";
+import {loadEnvConfig} from "@next/env"
+
+ const projectDir = process.cwd();
+ loadEnvConfig(projectDir);
+
+async function loadFakeData(numUsers = 10) {
+  console.log(`Executing load fake data. Generating ${numUsers} users`);
+
+  
+  console.log(process.env.POSTGRES_USER);
+  const client = new Client({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_NAME,
+    password: process.env.POSTGRES_PASSWORD,
+    port: parseInt(process.env.POSTGRES_PORT!),
+  });
+
+  await client.connect();
+  const res = await client.query("select 1");
+
+   console.log(res.command); 
+  await client.end();
+}
+
+loadFakeData();
+
+
