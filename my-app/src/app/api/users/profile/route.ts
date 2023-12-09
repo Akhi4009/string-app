f53d606f9e16bb303ac3fc9edad5fefa793f0a50
Akhilesh yadav <akhileshyadav310596@gmail.com>
2023-12-09 13:57:29 +0530

    profile endpoint jwtverify

---
+++ my-app/src/app/api/users/profile/route.ts
@@ -0,0 +1,23 @@
+import { getJWTpayload } from "@/app/util/auth";
+import { sql } from "../../../../../db";
+import { NextResponse } from "next/server";
+
+export async function GET(request:Request){
+
+    // Get current logged in user
+    const jwtPayload = await getJWTpayload();
+
+   // fetch user data
+
+   const res = await sql(
+    `select id, username, avatar from users where id=$1`,
+    [jwtPayload.sub]
+   );
+
+   const user = res.rows[0]
+
+    // return user data
+    
+    return NextResponse.json({data:user},{status:200})
+
+}
---
+++ my-app/src/app/util/auth.ts
@@ -0,0 +1,13 @@
+import { jwtVerify } from "jose";
+import {cookies} from "next/headers";
+
+export async function getJWTpayload(){
+    const cookieStore=cookies()
+    const token = cookieStore.get("jwt-token");
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET!);
+    const {payload,protectedHeader} = await jwtVerify(token?.value!,secret)
+
+    return payload
+}
+
+
