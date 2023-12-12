a4ea51e22d6e229067b96c975fb993eb5e786192
Akhilesh yadav <akhileshyadav310596@gmail.com>
2023-12-12 09:34:25 +0530

    get posts end point

---
+++ my-app/src/app/api/posts/[id]/route.ts
@@ -0,0 +1,21 @@
+import { getJWTpayload } from "@/app/util/auth";
+import { sql } from "../../../../../db";
+import { NextResponse } from "next/server";
+
+
+export async function GET(request:Request,
+    {params}:{params:{id:number}}){
+
+        const jwtPayload=await getJWTpayload();
+
+        const res = await sql(`select * from posts where id=$1 and user_id=$2`,
+        [params.id,jwtPayload.sub]);
+
+        if(res.rowCount===0){
+            return NextResponse.json({error:"Not found"},{status:404});
+        }
+    
+        return NextResponse.json({data:res.rows[0]})
+
+
+}
